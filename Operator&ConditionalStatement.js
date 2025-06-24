92bdc0adcb054dca763845619b9a256ffadb7f71
Mohit Sharma <115013643+ms593@users.noreply.github.com>
2025-06-24 22:45:47 +0530

    Add files via upload

---
+++ Operator&ConditionalStatement.js
@@ -0,0 +1,26 @@
+// let a=5;
+// let b=2;
+// console.log("a=",a," & b=",b);
+// console.log("a+b=",a+b)
+// console.log("a-b=",a-b)
+// console.log("a**b=",a**b)
+//Unary Operator
+//  let a=5;
+//  let b=2;
+//  console.log("a=",a," & b=",b);
+//  a++;
+//  console.log(a);
+ //Assignment Operator
+ //= += -= *= %= **=
+ //Comparison Operator
+ //== !==
+//  let a=5;
+//  console.log("a = ",a);
+//  let b='"5"';
+// console.log("b = ",b);
+//  console.log("a==b ",'5="5"',a==b,"Check only values");
+//  console.log("a===b",'5="5"',a===b,"Check Type as well","Stricter version");
+//logical operator in js
+let a=6;
+let b=5;
+console.log("!(6<5)=",!(a===6)); //false
